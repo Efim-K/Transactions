adcb205798fe4b1a9db3190ed96110aa18178410
Efim Kashapov <bambalabes@gmail.com>
2024-04-30 23:30:18 +0500

    Созданы две функции: на чтение базы данных и на вывод операций

---
+++ scr/utils.py
@@ -0,0 +1,76 @@
+import json
+from operator import itemgetter
+from zipfile import ZipFile
+
+
+def load_operation(data_file):
+    """
+    Загрузка данных из файла из архива и получение актуального списка последних операций
+    :param data_file: исходный zip файл с данными
+    :return: отсортированный список данных
+    """
+    # получаем данные из zip файла
+    with ZipFile(data_file, 'r') as zip_file:
+        name_file = zip_file.namelist()
+        with zip_file.open(name_file[0], 'r') as unzip_file:
+            operation_list = json.load(unzip_file)
+
+    # проверяем список словарей на соответствие загружаемых данных
+    operation_list_new = [i for i in operation_list if 'date' in i]
+
+    # сортируем список по дате
+    operation_list_sort = sorted(operation_list_new, key=itemgetter('date'), reverse=True)
+
+    return operation_list_sort
+
+
+def get_last_operation(operations, last_operation):
+    """
+    Создает строку с последними операциями, выполненных клиентом
+    :param operations: список операций клиента
+    :param last_operation: количество последних операций для вывода
+    :return: строка с последними операциями
+    """
+    operation_last_view = ''
+    index_operation = 0
+    # получаем список последних выполненных операций и преобразовываем для вывода
+    for i in range(len(operations)):
+        if operations[i]['state'] != 'EXECUTED':
+            continue
+
+        # добавляем в вывод дату операции
+        date = operations[i]['date'][:10]
+        operation_last_view += f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
+        # добавляем в вывод наименование операции
+        operation_last_view += f' {operations[i]['description']}\n'
+
+        # добавляем в вывод откуда пришел перевод
+        if "from" in operations[i]:
+            if operations[i]['from'][:4] == 'Счет':
+                operation_last_view += f'Счет **{operations[i]["from"][-4:]} '
+            else:
+                operation_last_view += (f'{operations[i]["from"][:-12]}'
+                                        f' {operations[i]["from"][-11:-9]}'
+                                        f'** **** {operations[i]["from"][-4:]} '
+                                        )
+
+        # добавляем в вывод куда пришел перевод
+        if "to" in operations[i]:
+            if operations[i]["to"][:4] == 'Счет':
+                operation_last_view += f'-> Счет **{operations[i]["to"][-4:]}\n'
+            else:
+                operation_last_view += (f'-> {operations[i]["to"][:-12]}'
+                                        f' {operations[i]["to"][-11:-9]}'
+                                        f'** **** {operations[i]["to"][-4:]}\n'
+                                        )
+
+        # добавляем в вывод сумму операции
+        operation_last_view += (f'{operations[i]["operationAmount"]["amount"]} '
+                                f'{operations[i]["operationAmount"]["currency"]["name"]}\n\n'
+                                )
+
+        index_operation += 1
+        if index_operation >= last_operation:
+            break
+
+    return operation_last_view
